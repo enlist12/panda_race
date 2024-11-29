1fbc5a2801e3e03c759b9cf36197f93f7298640e
haoqiguai <126598825+enlist12@users.noreply.github.com>
2024-11-29 19:15:30 +0800

    Create hello_world.c
    hello_world

---
+++ hello_world.c
@@ -0,0 +1,26 @@
+#define __STDC_FORMAT_MACROS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "panda/plugin.h"
+#include "panda/plugin_plugin.h"
+#include "panda/common.h"
+
+bool init_plugin(void *self);
+void uninit_plugin(void *self);
+void after_vmload(CPUState *env);
+
+void after_vmload(CPUState *env) {
+    puts("hello world");
+}
+
+bool init_plugin(void *self) {
+    panda_cb pcb;
+    pcb.after_loadvm = after_vmload; 
+    panda_register_callback(self, PANDA_CB_AFTER_LOADVM, pcb);
+    return true; 
+}
+
+void uninit_plugin(void *self) {
+    // nothing
+}
